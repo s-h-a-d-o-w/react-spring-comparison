7e2ba0f1aac7a39e9b99138c1673b6664fd6ef2a
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2024-09-22 13:21:18 +0200

    Add CD script

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 set -e
 
-export CAPROVER_APP=react-spring-comparison
-export CAPROVER_TAR_FILE=./caprover_deployment.tar
-
 yarn build
 tar -cvf ./caprover_deployment.tar ./Dockerfile ./build/*
 
+# Password and caprover machine URLs (which may change)
+[ -f ".env" ] && source ".env"
+
+export CAPROVER_APP=react-spring-comparison
+export CAPROVER_TAR_FILE=./caprover_deployment.tar
+
 export CAPROVER_URL=$CAPROVER_MACHINE_01
-caprover deploy
+npx caprover deploy > /dev/null
 
 export CAPROVER_URL=$CAPROVER_MACHINE_02
-caprover deploy
+npx caprover deploy > /dev/null
 
 rm caprover_deployment.tar
